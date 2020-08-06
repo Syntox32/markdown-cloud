59299e96c7d6173d3b53d681afe75ec685e418ca
Anders Milje <syntox32@gmail.com>
2020-08-06 23:14:14 +0200

    worker: rewrite all requests to /

--- workers-site/index.js
+++ workers-site/index.js
@@ -74,7 +74,7 @@ function handlePrefix(prefix) {
     let url = new URL(defaultAssetKey.url)
 
     // strip the prefix from the path for lookup
-    url.pathname = url.pathname.replace(prefix, '/')
+    url.pathname = '/' //url.pathname.replace(prefix, '/')
 
     // inherit all other props from the default request
     return new Request(url.toString(), defaultAssetKey)
